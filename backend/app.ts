f5d80d5ad8aaceb123e3e657b79d11ce01a4b32b
kartik191202 <pkartik1912@gmail.com>
2025-10-03 01:37:26 +0530

    Initial commit of mefn_note_app_deploy

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,20 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
 import express from "express";
 import cors from "cors";
 import { json } from "stream/consumers";
+import mongoose from "mongoose";
 import appLogger from "./src/middleware/app_logger"
 import { connectToDatabase } from "./src/config/mongodb_client";
 import userRouter from "./src/router/user_router";
 import noteRouter from "./src/router/note_router";
 // "cors" => cross origine resource sharing , with the help of this we gonna load data in our application
+
+
+
 
 const app : express.Application = express();
 
@@ -19,6 +28,15 @@ const portNumber = 8000;
 
 
 //const portNumber = process.env.PORT || 59242;
+
+const MONGO_URI = process.env.DB_URI;
+if (!MONGO_URI) {
+    throw new Error("DB_URI environment variable is not defined.");
+}
+mongoose.connect(MONGO_URI);
+
+
+
 
 app.use(cors())
 debugger
